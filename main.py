8c845a0b5408cba87cda9f461371de408ee23820
Aykut <76070532+aykutemreyalcin@users.noreply.github.com>
2025-01-15 14:00:08 +0100

    Update main.py
    adding the student class and its functions

--- main.py
+++ main.py
@@ -22,3 +22,14 @@ communication_queue = []
 def emmit_event(event):
     communication_queue.append(event)
     print("Event {} emitted".format(event.name))
+
+class Student:
+    def __init_(self, name, transcript_note):
+        self.name = name
+        self.transcript_note = transcript_note
+
+    def apply_for_uni(self):
+        payload = {"name":self.name, "transcript_note":self.transcript_note}
+        event = ApplicationSent(payload)
+        emmit_event(event)
+        
