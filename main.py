5592fcf7c506e49a2885ad6367cc1c7b97f5ae3b
Aykut <76070532+aykutemreyalcin@users.noreply.github.com>
2025-01-15 13:52:59 +0100

    Update main.py
    creating the subclasses using inheritance by the Event class

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 class Event:
     def __init__(self, payload):
         self.payload = payload
+        
+class ApplicationSent(Event):
+    name = "applicaiton_sent"
+    def __init__(self, payload):
+        super().__init__(payload)
+
+class ApplicationAccepted(Event):
+    name = "application_accepted"
+    def __init__(self, payload):
+        super().__init__(payload)
+
+class ApplicationRejected(Event):
+    name = "application_rejected"
+    def __init__(self, payload):
+        super().__init__(payload)
+   
